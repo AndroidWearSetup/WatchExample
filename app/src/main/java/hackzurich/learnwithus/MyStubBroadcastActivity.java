e469410d084c841d3cb73fb71b437a80a35fb4f7
ghost <kas_kon5@wp.pl>
2015-09-05 11:37:54 +0200

    using jobScheduler to display toast :D

--- app/src/main/java/hackzurich/learnwithus/MyStubBroadcastActivity.java
+++ app/src/main/java/hackzurich/learnwithus/MyStubBroadcastActivity.java
@@ -1,6 +1,10 @@
 package hackzurich.learnwithus;
 
 import android.app.Activity;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -8,6 +12,7 @@ import android.os.Bundle;
  * Example shell activity which simply broadcasts to our receiver and exits.
  */
 public class MyStubBroadcastActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,14 @@ public class MyStubBroadcastActivity extends Activity {
         i.setAction("hackzurich.learnwithus.SHOW_NOTIFICATION");
         i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
         sendBroadcast(i);
-        finish();
+
+        JobScheduler mJobScheduler = (JobScheduler)
+                getSystemService(Context.JOB_SCHEDULER_SERVICE);
+
+        JobInfo.Builder builder = new JobInfo.Builder(1,new ComponentName(getPackageName(), JobSchedulerService.class.getName()))
+                .setPeriodic(5_000);
+        if( mJobScheduler.schedule( builder.build() ) <= 0 ) {
+            throw new RuntimeException("scheduling failed");
+        }
     }
 }
