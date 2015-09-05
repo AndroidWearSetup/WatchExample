438fc52c4e45cbfbb8001f74ff7cecf7b7d242fa
ordon <ordon1313@gmail.com>
2015-09-05 10:58:53 +0200

    retrolambda

--- app/src/main/java/hackzurich/learnwithus/MyDisplayActivity.java
+++ app/src/main/java/hackzurich/learnwithus/MyDisplayActivity.java
@@ -3,6 +3,7 @@ package hackzurich.learnwithus;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MyDisplayActivity extends Activity {
 
@@ -10,11 +11,8 @@ public class MyDisplayActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
-        findViewById(R.id.display).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
+        findViewById(R.id.display).setOnClickListener(v -> {
+            Toast.makeText(this,"aaaaa",Toast.LENGTH_LONG).show();
         });
     }
 }
